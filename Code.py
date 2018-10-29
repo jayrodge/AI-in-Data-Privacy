0cc4d2f8396e1925332d691e48130f7a1e6b1fcc
Aman Agarwal <amanagarwal9891@gmail.com>
2018-10-29 00:52:29 -0500

    Code file updated

--- Code.py
+++ Code.py
@@ -24,15 +24,15 @@ for index in tqdm(range(len(filenames))):
     filtered_sentence = multi_replace(filtered_sentence, replacements)
     filtered_sentence = str(filtered_sentence).lower()
     dataset.loc[index] = [int(re.findall('\d+', filename)[0]), filtered_sentence]
-    
+
 dataset["id"] = dataset["id"].astype("uint32")
 y["id"] = y["id"].astype("uint32")
-
 final_dataset = pd.merge(dataset, y, on='id')
-final_dataset["score"] = final_dataset["score"].astype("uint8")
+
+final_dataset = final_dataset.drop_duplicates(subset=["text"])
 
 final_dataset['word_count'] = final_dataset['text'].apply(lambda x: word_count(x))
-final_dataset["sentence_count"] = final_dataset['text'].apply(lambda x: sentence_count(x))
+final_dataset['sentence_count'] = final_dataset['text'].apply(lambda x: sentence_count(x))
 final_dataset['avg_sentence_length'] = final_dataset['word_count'].astype("float")/final_dataset['sentence_count'].astype("float")
 final_dataset['syllables_count'] = final_dataset['text'].apply(lambda x: syllables_count(x))
 final_dataset['avg_syllables_per_word'] = final_dataset['text'].apply(lambda x: avg_syllables_per_word(x))
@@ -42,5 +42,21 @@ final_dataset['flesch_reading_ease'] = final_dataset['text'].apply(lambda x: fle
 final_dataset['gunning_fog'] = final_dataset['text'].apply(lambda x: gunning_fog(x))
 final_dataset['smog_index'] = final_dataset['text'].apply(lambda x: smog_index(x))
 final_dataset['dale_chall_readability_score'] = final_dataset['text'].apply(lambda x: dale_chall_readability_score(x))
+
+minor_synonyms = set(['minor', 'child', 'kid', 'youngster'])
+geolocation_synonyms = set(['geo-location', 'geolocation', 'location', 'position'])
+vendor_synonyms = set(['vendor', 'vender', 'seller', 'marketer'])
+
+final_dataset['number_of_email_addresses'] = final_dataset['text'].apply(lambda x: number_of_email_addresses(x))
+final_dataset['use_cookies'] = final_dataset['text'].apply(lambda x: use_cookies(x))
+
+# final_dataset['is_minor'] = final_dataset['text'].apply(lambda x: is_minor(x))
+# final_dataset['is_how_collect'] = 
+# final_dataset['is_geo_location'] = 
+# final_dataset['is_vendor'] = 
+# final_dataset['is_not_sell'] = 
+# final_dataset['is_sell'] = 
+# final_dataset['is_share'] = 
+# final_dataset['is_not_share'] = 
 
 final_dataset.to_csv("final_dataset.csv", encoding='utf-8', index = False)
